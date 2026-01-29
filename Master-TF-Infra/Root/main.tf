3138673c4037f0ac4d25ebe61254f0b60aa838f2
Dhanunjay144 <bdhanunjayardy@gmail.com>
2026-01-29 06:40:14 +0530

    added all the variables to inputjson file for variable input from user

---
+++ Master-TF-Infra/Root/main.tf
@@ -0,0 +1,47 @@
+# Module for Resource Group
+module "ResourceGroup" {
+  source   = "..//Modules/ResourceGroup"
+  rgname   = "${var.ProjectName}-${var.Loc}-${var.Environment}-rg-01"
+  location = var.Location
+  default_tags     = var.default_tags
+}
+
+# Module for Network
+module "Network" {
+  source      = "..//Modules/Network"
+  depends_on = [ module.ResourceGroup ]
+  rgname      = "${var.ProjectName}-${var.Loc}-${var.Environment}-rg-01"
+  vnetname    = "${var.ProjectName}-${var.Loc}-${var.Environment}-vnet"
+  snetname    = "${var.ProjectName}-${var.Loc}-${var.Environment}-snet"
+  vnetaddress = var.vnet_address_space
+  snetaddress = var.subnet_address_space
+  default_tags = var.default_tags
+}
+
+module "AKS" {
+  source      = "..//Modules/AKS"
+  depends_on = [ module.Network ]
+  rgname                 = "${var.ProjectName}-${var.Loc}-${var.Environment}-rg-01"
+  vnetname               = "${var.ProjectName}-${var.Loc}-${var.Environment}-vnet"
+  snetname               = "${var.ProjectName}-${var.Loc}-${var.Environment}-snet"
+  aksname                = "${var.ProjectName}-${var.Loc}-${var.Environment}-aks"
+  aksdnsprefix           = "${var.ProjectName}-${var.Loc}-${var.Environment}-aks-dns"
+  aks_pricing_tier       = var.aks_pricing_tier
+  aks_kubernetes_version = var.aks_kubernetes_version
+  aks_node_rgname        = "${var.ProjectName}-${var.Loc}-${var.Environment}-aks-node-rg" # Functions in terraform substr
+  aks_nodepool_name      = "${substr(var.ProjectName, 0, 3)}${var.Environment}aks"
+  aks_node_count         = var.aks_node_count
+  aks_nodepool_sku       = var.aks_nodepool_sku
+  availability_zones    = var.availability_zones
+  max_pods_per_node     = var.max_pods_per_node
+  aks_service_cidr      = var.aks_service_cidr
+  aks_dns_service_ip    = var.aks_dns_service_ip
+  default_tags          = var.default_tags
+}
+
+# Output the AKS cluster kubeconfig
+output "kubeconfig" {
+  description = "Kubeconfig for the AKS cluster"
+  value       = module.AKS.kubeconfig
+  sensitive   = true
+}
