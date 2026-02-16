d210f9196d6833a6e5e76eb87474b9204524ba2a
Dhanunjay144 <bdhanunjayardy@gmail.com>
2026-02-16 22:17:50 +0530

    Adding ACR to store application Images

--- Master-TF-Infra/Modules/ACR/acr.tf
+++ Master-TF-Infra/Modules/ACR/acr.tf
@@ -2,3 +2,10 @@ data "azurerm_resource_group" "rg" {
   name = var.rgname
 }
 
+resource "azurerm_container_registry" "acr" {
+  name                = var.acrname
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  sku                 = var.acrsku
+  admin_enabled       = var.adminenabled
+}
--- Master-TF-Infra/Modules/ACR/variables.tf
+++ Master-TF-Infra/Modules/ACR/variables.tf
@@ -2,3 +2,15 @@ variable "rgname" {
   type = string
   description = "Name of the resource group"
 }
+variable "acrname" {
+  type = string
+  description = "Name of the Azure Container Registry"
+}
+variable "acrsku" {
+  type = string
+  description = "SKU of the Azure Container Registry (Basic, Standard, Premium)"
+}
+variable "adminenabled" {
+  type = bool
+  description = "Enable admin user for ACR (true/false)"
+}
